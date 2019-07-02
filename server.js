691033ca52826883f0fb7d513edf8018bbdc6094
Noam <shvachernoam@gmail.com>
2019-07-02 10:18:09 +0300

    trying utc

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.get('/echoAtTime', function(req, res) {
 
     setTimeout(() => {
         var timeOnPrint = time;
-        console.log(moment(time * 1000).format("DD/MM/YYYY hh:mm:ss"));
+        console.log(moment(time * 1000).utc().format("DD/MM/YYYY hh:mm:ss"));
         console.log(moment(timeOnPrint * 1000).local().format("DD/MM/YYYY hh:mm:ss"));
         printAndRemoveFromRedis(timeOnPrint, timeOnPrint);
         
