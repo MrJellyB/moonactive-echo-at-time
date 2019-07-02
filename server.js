1a0214d2199046e5396c3c5cb34d02bc183ac345
Noam <shvachernoam@gmail.com>
2019-07-02 09:53:03 +0300

    added time print

--- server.js
+++ server.js
@@ -56,6 +56,7 @@ app.get('/echoAtTime', function(req, res) {
 
     setTimeout(() => {
         var timeOnPrint = time;
+        console.log(moment(timeOnPrint * 1000).format("DD/MM/YYYY hh:mm:ss"));
         printAndRemoveFromRedis(timeOnPrint, timeOnPrint);
         
     }, getDeltaTimeFromNow(time));
